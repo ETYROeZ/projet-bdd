0db53499035401317654869997bb3eba61ac4c40
Vlad <vladazeaze@gmail.com>
2025-12-05 18:37:36 +0100

    Mise a Jour

--- req_05.sql
+++ req_05.sql
@@ -1,14 +1,14 @@
 /* Quel est le revenu mensuel d’un store donné le long d’une année ? */
 SELECT
-EXTRACT(MONTH FROM a.date_achat) AS mois,
-SUM(ap.prix) AS revenu_mensuel
+    EXTRACT(MONTH FROM a.date_achat) AS mois,
+    SUM(ap.prix) AS revenu_mensuel
 FROM
-ACHETER a, APPLICATION ap
+    ACHETER a, APPLICATION ap
 WHERE
-a.id_application = ap.id
-AND ap.id_store = :store_id
-AND EXTRACT(YEAR FROM a.date_achat) = :annee
+    a.id_application = ap.id
+    AND ap.id_store = :store_id
+    AND EXTRACT(YEAR FROM a.date_achat) = :annee
 GROUP BY
-EXTRACT(MONTH FROM a.date_achat)
+    EXTRACT(MONTH FROM a.date_achat)
 ORDER BY
-mois;
+    mois;
